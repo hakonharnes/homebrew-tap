b178753a8b805f5055af3a27241cbccceed68984
Håkon Harnes <hakon@harnes.co>
2025-05-19 21:17:43 +0200

    chore: update pbctl to v0.1.1

--- Formula/pbctl.rb
+++ Formula/pbctl.rb
@@ -1,8 +1,8 @@
 class Pbctl < Formula
   desc "Command-line interface to the MacOS pasteboard"
   homepage "https://github.com/hakonharnes/pbctl"
-  url "https://github.com/hakonharnes/pbctl/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0fdf8d970780c52589ff609c79bef0ca4228c495566214cacba82551e6c73a6f"
+  url "https://github.com/hakonharnes/pbctl/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "70a48c56ed0b38d2e28e484d6f2b9d4a17f69da98cfa2a738fe6074454172736"
   license "MIT"
   head "https://github.com/hakonharnes/pbctl.git", branch: "main"
 
