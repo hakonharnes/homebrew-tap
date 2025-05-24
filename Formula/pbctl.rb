04a43ecf72b14f3bbc7a281e8c6ac336034f51ad
Håkon Harnes <hakon@harnes.co>
2025-05-24 09:38:08 +0200

    fix: update SHA256 for v0.3.2 and fix workflow sed pattern

--- Formula/pbctl.rb
+++ Formula/pbctl.rb
@@ -2,7 +2,7 @@ class Pbctl < Formula
   desc     "Command-line interface to the macOS pasteboard"
   homepage "https://github.com/hakonharnes/pbctl"
   url      "https://github.com/hakonharnes/pbctl/archive/refs/tags/v0.3.2.tar.gz"
-  sha256   "256b6345ce799e2b2990a9c43127345909fe3a3ab44bbdd962781c4efb47d6c8"
+  sha256   "a3719e0fa76e8a8f5299e300b95572bcf741e53ed73f0e5179395d4cd94e7bbc"
   license  "MIT"
   head     "https://github.com/hakonharnes/pbctl.git", branch: "main"
 
